bb14b5e2c1eaf9792ab15832827412664057b645
EllaTretiakova <132052175+EllaTretiakova@users.noreply.github.com>
2024-05-08 21:38:35 +0300

    Add files via upload

---
+++ Lesson_3/route.rb
@@ -0,0 +1,25 @@
+# Класс Route (Маршрут):
+# Имеет начальную и конечную станцию, а также список промежуточных станций. Начальная и конечная станции указываютсся при создании маршрута, а промежуточные могут добавляться между ними.
+# Может добавлять промежуточную станцию в список
+# Может удалять промежуточную станцию из списка
+# Может выводить список всех станций по-порядку от начальной до конечной
+
+class Route
+	attr_reader :stations
+
+	def initiliaze (start, finish)
+		@stations = [start, finish]
+	end
+	def add_station (station, place)
+		@stations.insert(place, station)
+	end
+	def delete_station (station)
+		@stations.delete (station)
+	end
+	def next_station
+  		@station = route.stations[@route.stations.index(station) + 1]
+    end
+  	def previous_station
+     	@stations = route.stations[@route.stations.index(station) - 1]
+  end
+end
---
+++ Lesson_3/train.rb
@@ -0,0 +1,68 @@
+#Класс Train (Поезд):
+#Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса
+#Может набирать скорость
+#Может возвращать текущую скорость
+#Может тормозить (сбрасывать скорость до нуля)
+#Может возвращать количество вагонов
+#Может прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает или уменьшает количество вагонов). Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
+#Может принимать маршрут следования (объект класса Route). 
+#При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
+#Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
+#Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
+
+class Train
+	
+	attr_reader :number
+	attr_reader :type
+	attr_accessor :van
+	attr_accessor :speed
+	attr_reader :route
+
+	def initialize (type, number, van)
+		@number = number
+		@type = type
+		@van = van
+		@speed = 0
+	end
+	def speed_speed
+		@speed += 10 
+    end
+	def new_van
+		@van += 1 if @speed == 0
+	end
+	def delete_van
+		@van -=1 if @speed == 0
+	end
+	def train_route(route)
+ 		@station = route.station.first
+ 		@route = route
+	end
+	def stop
+		@speed = 0 if @speed > 0
+	end 
+ 	def moving_forward
+    	moving_to(next_station)
+ 	end
+  	def moving_back
+    	moving_to(previous_station)
+  	end
+ 	def next_station
+    	@route.next_station(current_station)
+  	end
+  	def previous_station
+    	@route.previous_station(current_station)
+  	end
+  	def moving_to(station)
+    	@current_station&.send(self)
+    	@current_station = station
+    	@current_station.take(self)
+   		true
+  	end
+end
+
+
+
+
+
+
+	 
